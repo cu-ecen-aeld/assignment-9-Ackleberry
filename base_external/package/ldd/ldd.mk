b80bc94a0d35f00683b1bb2f6549709bd99178a8
Brooks Anderson <brooks.anderson@snapone.com>
2024-11-24 18:49:11 -0600

    Seems to work now

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-LDD_VERSION = 1e066c6ea43a2a7b7e20c34b4a91dc22d980cc51
+LDD_VERSION = d62137d33f14a520e3a5d43856655be60181a837
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -18,4 +18,12 @@ LDD_MODULE_SUBDIRS = misc-modules
 LDD_MODULE_SUBDIRS += scull
 LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
 
+define LDD_INSTALL_TARGET_CMDS
+    $(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
+endef
+
 $(eval $(kernel-module))
+$(eval $(generic-package))
