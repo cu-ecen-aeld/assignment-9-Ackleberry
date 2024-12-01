bd5fe19781305298ac68f908c7ba65ec414e3edf
Brooks Anderson <brooks.anderson@snapone.com>
2024-12-01 15:42:21 -0600

    Buildroot now builds assignment 8 skeleton

---
+++ base_external/package/aesd-char-driver/aesd-char-driver.mk
@@ -0,0 +1,26 @@
+
+##############################################################
+#
+# AESD Char Driver - Assignment 8
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+AESD_CHAR_DRIVER_VERSION = cf325b3d9db267b03e4b3967d86b0832f99bfaeb
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESD_CHAR_DRIVER_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-Ackleberry.git
+AESD_CHAR_DRIVER_SITE_METHOD = git
+AESD_CHAR_DRIVER_SUBMODULES = YES
+AESD_CHAR_DRIVER_MODULE_SUBDIRS = aesd-char-driver
+AESD_CHAR_DRIVER_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
+
+define AESD_CHAR_DRIVER_INSTALL_TARGET_CMDS
+    $(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8-buildroot/* $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
